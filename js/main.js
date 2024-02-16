01ec46d55cfcb5e8d912aba0a555e8d7613c9f29
Dannie Vinther <davi@kea.dk>
2024-02-16 15:30:24 +0100

    Add random number generation and color generation for paragraphs

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ import { fetchData } from "./utils/fetch.js";
 
 import { setItem, getItem } from "./utils/storage.js";
 
+// Generate random numbers and colors for the paragraphs
+// $ and $$ are imported from dom.js
+// randomNumber and getRandomColor are imported from numbers.js and colors.js
 const ps = $$("p");
 const btn = $("button");
 const showNumber = $(".show-number");
@@ -22,8 +25,12 @@ btn.addEventListener("click", (e) => {
   populatePs(30);
 });
 
+// Split the text of the strings
+// splitText is imported from dom.js
 splitText("[data-split]");
 
+// Fetch data and display it
+// fetchData is imported from fetch.js
 async function showData() {
   const currentNumber = await fetchData(
     "https://kea-alt-del.dk/kata-distortion/"
@@ -35,6 +42,8 @@ showData();
 
 setInterval(showData, 5000);
 
+// Dark mode toggle
+// setItem and getItem are imported from storage.js
 const darkModeToggle = $("#darkmode");
 
 let isDarkMode = getItem("dark-mode");
